cc933d4e40c8fc37869ad0330bfca598713e6ede
Eugene O'Fallon <epofallon@gmail.com>
2021-02-03 02:34:45 +0000

    Update for current state

--- user_input.rb
+++ user_input.rb
@@ -1,13 +1,22 @@
-PASSWORD = 'asdf'
-USERNAME = 'epov'
-
-loop do
-  puts '>> Please enter user name:'
-  usrnm = gets.chomp
-  puts '>> Please enter your password:'
-  psswrd = gets.chomp
-  break if usrnm == USERNAME && psswrd == PASSWORD
-  puts '>> Authorization failed!'
+def valid_number?(number_string)
+  number_string.to_i.to_s == number_string && number_string.to_i != 0
 end
 
-puts '>> Welcome!'
+def user_input
+  puts '>> Please enter a positive or negative integer'
+  num = gets.chomp
+  return num if valid_number?(num)
+  puts 'Invalid input. Only non-zero integers are allowed.'
+end
+
+numbers []
+input = nil
+
+loop do
+  input = user_input
+  if input
+    numbers << input
+    count += 1
+  end
+  
+end
