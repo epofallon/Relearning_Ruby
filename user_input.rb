d6104d6cd80d42681d0b815513bfe4303127c326
Eugene O'Fallon <epofallon@gmail.com>
2021-02-03 19:01:31 +0000

    update user input file

--- user_input.rb
+++ user_input.rb
@@ -2,21 +2,38 @@ def valid_number?(number_string)
   number_string.to_i.to_s == number_string && number_string.to_i != 0
 end
 
-def user_input
-  puts '>> Please enter a positive or negative integer'
-  num = gets.chomp
-  return num if valid_number?(num)
-  puts 'Invalid input. Only non-zero integers are allowed.'
+def test_pos_neg(arr)
+  test_arr = []
+  arr.each {|i| test_arr << i.abs}
+  return true if arr.sum.abs < test_arr.sum
 end
 
-numbers []
-input = nil
+
+
+num = []
 
 loop do
-  input = user_input
-  if input
-    numbers << input
-    count += 1
+  puts '>> Please enter a positive or negative integer:'
+  number = gets.chomp
+  
+  if valid_number?(number) == false
+    puts '>> Invalid input. Only non-zero integers are allowed.'
+    next
+  end
+  
+  num << number.to_i
+  
+  if num.length == 2
+    pass = test_pos_neg(num)
+    if pass == true
+      break
+    else
+      puts '>> Sorry. One integer must be positive, one must be negative'
+      puts '>> Please start over'
+      num = []
+    end
   end
   
 end
+
+puts "#{num[0]} + #{num[1]} = #{num.sum}"
